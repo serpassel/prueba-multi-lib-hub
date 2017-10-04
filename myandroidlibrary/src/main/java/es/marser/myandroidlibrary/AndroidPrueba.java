a8bd03a6adfd606631b4f563fbd354b4f7abd7e7
sergio <spseligrat@gmail.com>
2017-10-04 08:15:01 +0200

    Pruabas de android y Java

---
+++ myandroidlibrary/src/main/java/es/marser/myandroidlibrary/AndroidPrueba.java
@@ -0,0 +1,9 @@
+package es.marser.myandroidlibrary;
+
+/**
+ * @author sergio
+ *         Created by sergio on 4/10/17.
+ */
+
+public class AndroidPrueba {
+}
---
+++ myjavalibrary/src/main/java/com/example/JavaPrueba.java
@@ -0,0 +1,9 @@
+package com.example;
+
+/**
+ * @author sergio
+ *         Created by sergio on 4/10/17.
+ */
+
+public class JavaPrueba {
+}
